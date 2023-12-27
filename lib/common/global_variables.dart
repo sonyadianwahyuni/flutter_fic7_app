56a4a1dcd390b900a15d92168ddef26d9fc599d7
sonyadianwahyuni <sonyadianwahyuni@gmail.com>
2023-12-27 20:14:31 +0700

    meet 11

--- lib/common/global_variables.dart
+++ lib/common/global_variables.dart
@@ -1,4 +1,3 @@
 class GlobalVariables {
-  static const baseUrl =
-      'https://9407-2404-8000-1028-1268-fc39-9e83-5280-55cf.ngrok-free.app';
+  static const baseUrl = 'https://5797-113-11-183-209.ngrok-free.app';
 }
